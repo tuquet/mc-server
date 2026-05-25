3e94f83a2dc12be464eb601c6b3c011ca5348d42
Toby Nguyen (Nguyễn Đình Tú) <tunyk.93@gmail.com>
2026-05-25 10:32:36 +0700

    fix: update restart-script command for compatibility with Windows

--- run.bat
+++ run.bat
@@ -5,10 +5,17 @@ set RAM_MAX=4G
 set RAM_MIN=4G
 set JAR_NAME=paper.jar
 
+:start
+cls
 echo Dang khoi dong Minecraft Paper Server voi %RAM_MAX% RAM...
 echo Su dung Aikar's Flags de toi uu hoa va giam giat lag Java...
 
 "%APPDATA%\.minecraft\runtime\java-runtime-epsilon\windows\java-runtime-epsilon\bin\java.exe" -Xmx%RAM_MAX% -Xms%RAM_MIN% -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8m -XX:G1ReservePercent=15 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -jar %JAR_NAME% nogui
 
-echo Server da dung. Nhan phim bat ky de thoat.
-pause
+echo.
+echo Server da dung. Tu dong khoi dong lai sau 5 giay...
+echo Neu muon tat han server, hay bam Ctrl + C de ngat.
+echo.
+timeout /t 5
+
+goto start
